501a7bdb64678b298d97bcf9518d934f76b544fe
Artyom Fomenko <a.fomenko@mafin.ru>
2019-10-19 18:31:56 +0300

    Fixed posthook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == '__main__':
     print("Do not forget to initialize git project inside {{cookiecutter.project_slug}}!")
-    print("cd {{cookiecutter.project_slug}}} && git init")
+    print("cd {{cookiecutter.project_slug}} && git init")
